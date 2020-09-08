fff2937029e9f619406af554387fd34da66ebabd
Paul Letkeman <mojo_jojo13@pltk.de>
2020-09-08 12:23:50 +0200

    fix config

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-export interface Options {
-    rootDir: string;
-    targetDir: string;
-    scanDir: string;
-    nodeModulesDir: string;
-    exclude: string[];
-    fileExtensionFilter: string[];
-}
--- dist/util/util.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Prints recursively all children nodes of given node as tree.
- */
-export declare function printChildren(node: any, indent?: string): void;
